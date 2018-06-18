612e27b2e8b878bb0d2a5e5381db7a5bea7a3508
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2018-06-18 20:43:11 +0200

    add time in message

--- src/main/java/pl/krzysztofbujak/randomchat/models/ChatSocket.java
+++ src/main/java/pl/krzysztofbujak/randomchat/models/ChatSocket.java
@@ -9,19 +9,25 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
 
     private List<WebSocketSession> userList = new ArrayList<>();
+    private Queue<String> lastTenMessages = new ArrayDeque<>();
+
+    private TextMessage getActualTime() {
+        Calendar d = Calendar.getInstance();
+        return new TextMessage("(" + d.get(Calendar.HOUR) + ":" + d.get(Calendar.MINUTE) + ":" + d.get(Calendar.SECOND) + ") ");
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        addMessageToQue(getActualTime().getPayload() + message.getPayload());
         userList.forEach(s -> {
             try {
-                s.sendMessage(new TextMessage(message.getPayload()));
+                s.sendMessage(new TextMessage(getActualTime().getPayload()+message.getPayload()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -36,11 +42,27 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         userList.add(session);
+
+        lastTenMessages.forEach(s -> {
+            try {
+                session.sendMessage(new TextMessage(s));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(this, "/room")
                 .setAllowedOrigins("*");
+    }
+
+    private void addMessageToQue(String message) {
+        if (lastTenMessages.size() >= 10) {
+            lastTenMessages.poll();
+        }
+
+        lastTenMessages.offer(message);
     }
 }
